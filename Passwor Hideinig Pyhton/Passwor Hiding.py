40e37c6bffd8208d4b55951437c9ea93a91fe872
Prince Singh <thisisprincesingh@gmail.com>
2023-02-24 19:27:13 +0530

    Add files via upload
    add all files

---
+++ Passwor Hideinig Pyhton/Passwor Hiding.py
@@ -0,0 +1,11 @@
+# from getpass import getpass
+# user = input("Enter UserId: ")
+# Pass = getpass("Enter Password: ")
+# print(user)
+# print(Pass)
+from getpass import getpass
+
+
+password = getpass('Password: ')
+
+print(password)
